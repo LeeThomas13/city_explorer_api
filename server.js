d33911b0d8909cd061e2ad2bc09a39df6934d088
leethomas <lmtthomasritz@gmail.com>
2020-09-15 18:40:57 -0700

    finished rendering weather forecast for selected city.

--- server.js
+++ server.js
@@ -16,12 +16,11 @@ app.use(cors());
 //Route Definitions
 app.get('/', blankHandler);
 app.get('/location', locationHandler);
-// app.get('/weather', weatherHandler);
-// app.get('/trails', trailHandler);
+app.get('/weather', weatherHandler);
+app.get('/trails', trailHandler);
 app.use('*', brokenHandler);
 
-//Functions
-
+// Location Functions
 function locationHandler (request, response) {
     let city = request.query.city;
     let key = process.env.GEOCODE_API_KEY;
@@ -41,14 +40,6 @@ function locationHandler (request, response) {
         })
 }
 
-// function weatherHandler (request, response) {
-    
-// }
-
-// function trailHandler (request, response) {
-
-// }
-
 function ConstructCity (city, geoData) {
     this.search_query = city;
     this.formatted_query = geoData.display_name;
@@ -56,14 +47,45 @@ function ConstructCity (city, geoData) {
     this.longitude = geoData.lon;
 }
 
-// function ConstructWeather (city, weatherObject) {
-    
-// }
+//Weather Functions
+function weatherHandler (request, response) {
+    let lat = request.query.latitude;
+    let lon = request.query.longitude;
+    console.log(request.query)
+    let key = process.env.WEATHER_API_KEY;
+    const url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${key}&lat=${lat}&lon=${lon}&days=8`;
 
-// function ConstructTrails (city, trailObject) {
 
-// }
+    superagent.get(url)
+        .then((val) => {
+            //to console.log the incoming API
+            // console.log(val.body)
+            const weatherData = val.body.data.map(obj => {
+                return new ConstructWeather(obj)
+            })
+            response.send(weatherData)
+        })
+        .catch((error) => {
+            console.log('ERROR', error);
+            response.status(500).send('this is broke AF');
+        })
+}
 
+function ConstructWeather (weatherObject) {
+    this.forecast = weatherObject.weather.description;
+    this.time = weatherObject.datetime;
+}
+
+//Trail Functions
+function trailHandler (request, response) {
+
+}
+
+function ConstructTrails (city, trailObject) {
+
+}
+
+//Error Functions
 function blankHandler (request, response) {
     response.send('Hello World');
 }
